93d0a445d9e6482351d7445e6a8580927b017a53
ItamarKalmanson <itamar.kalmanson@gmail.com>
2026-03-03 16:40:43 +0200

    changed filenames

--- STIPA-with-pyscreech.py
+++ STIPA-with-pyscreech.py
@@ -14,20 +14,20 @@ def get_stipa_from_library(clean_file, recorded_file):
         "--log-details"
     ]
 
-    try:
+    # try:
         # השינוי כאן: איחוד ערוץ הלוגים לתוך ערוץ ההדפסה
-        result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, encoding='utf-8',
+    result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True, encoding='utf-8',
                                 check=True)
-        print("\n=== תוצאות הספרייה ===")
-        print(result.stdout)
+    print("\n=== תוצאות הספרייה ===")
+    print(result.stdout)
 
-    except subprocess.CalledProcessError as e:
-        print("\n=== שגיאה מהספרייה ===")
-        print(e.stdout)
+    # except subprocess.CalledProcessError as e:
+    #     print("\n=== שגיאה מהספרייה ===")
+    #     print(e.stdout)
 
 
 if __name__ == "__main__":
-    clean_audio = r"C:\Users\itama\OneDrive\שולחן העבודה\STIPA\STIPA ref.wav"
-    toy_audio = r"C:\Users\itama\OneDrive\שולחן העבודה\STIPA\מחוץ לחדר.wav"
+    clean_audio = r"C:\Users\itama\OneDrive\מסמכים\GitHub\STIPA-python\STIPA-recs\part2\aligned\FULL_ALIGNED_MASTER_SYNC_STIPA6 מטר למטה.wav"
+    toy_audio = r"C:\Users\itama\OneDrive\מסמכים\GitHub\STIPA-python\STIPA-recs\part2\aligned\FULL_ALIGNED_6 מטר למטה.wav"
 
     get_stipa_from_library(clean_audio, toy_audio)
